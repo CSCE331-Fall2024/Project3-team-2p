343d82106aa3c3391a8e7a501a5d4bcfdc9ec554
murphymy04 <137734306+murphymy04@users.noreply.github.com>
2024-10-30 15:30:32 -0500

    Made changes to gitignore file

--- server/db_connection.js
+++ server/db_connection.js
@@ -1,14 +1,17 @@
 import pkg from 'pg';
+import dotenv from 'dotenv';
 const { Pool } = pkg;
+
+dotenv.config();
 
 class DBConnection {
     constructor() {
         this.pool = new Pool({
-            host: "csce-315-db.engr.tamu.edu",
-            user: "team_2p",
-            password: "pawmo",
-            database: "team_2p_db",
-            port: 5432,
+            host: process.env.HOST,
+            user: process.env.USER,
+            password: process.env.PASSWORD,
+            database: process.env.DATABASE,
+            port: process.env.DBPORT,
           });
     }
 }
--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-app.use(cors({ origin: 'http://localhost:8080' }));
+app.use(cors());
 app.use(express.json());
 app.use('/api/customers', customerRoutes);
 app.use('/api/inventory', inventoryRoutes);
