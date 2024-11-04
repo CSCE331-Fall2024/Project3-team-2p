33974125755ed2f069d67642feeffb6121161317
murphymy04 <137734306+murphymy04@users.noreply.github.com>
2024-11-04 15:25:27 -0600

    fixed semicolons

--- client/src/components/manager_views/ManagerEmployees.vue
+++ client/src/components/manager_views/ManagerEmployees.vue
@@ -45,7 +45,7 @@
 
 <script>
 import axios from 'axios';
-axios.defaults.baseURL = process.env.VUE_APP_BASE_URL;;
+axios.defaults.baseURL = process.env.VUE_APP_BASE_URL;
 export default {
     data() {
         //TODO: replace with data from api call
--- client/src/components/manager_views/ManagerMenu.vue
+++ client/src/components/manager_views/ManagerMenu.vue
@@ -42,7 +42,7 @@
 
 <script>
 import axios from 'axios';
-axios.defaults.baseURL = process.env.VUE_APP_BASE_URL;;
+axios.defaults.baseURL = process.env.VUE_APP_BASE_URL;
 export default {
     data() {
         //TODO: replace with data from api call
