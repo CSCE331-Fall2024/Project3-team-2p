29862704dce33bb9ae01680a07869cbbac92e19b
timtakcs <timmurik@outlook.com>
2024-11-07 12:19:35 -0600

    fixed issue with env file in server

--- server/db_connection.js
+++ server/db_connection.js
@@ -1,18 +1,19 @@
 import pkg from 'pg';
 import dotenv from 'dotenv';
-const { Pool } = pkg;
 
 dotenv.config();
+
+const { Pool } = pkg;
 
 class DBConnection {
     constructor() {
         this.pool = new Pool({
             host: process.env.HOST,
-            user: process.env.USER,
+            user: process.env.DBUSER,
             password: process.env.PASSWORD,
             database: process.env.DATABASE,
             port: process.env.DBPORT,
-          });
+        });
     }
 }
 
