494884fcf18355f3b80d6fe801b0565cfa34957f
murphymy04 <137734306+murphymy04@users.noreply.github.com>
2024-11-13 21:46:20 -0600

    Fixed environment variables

--- server/db_connection.js
+++ server/db_connection.js
@@ -5,28 +5,16 @@ dotenv.config();
 
 const { Pool } = pkg;
 
-// class DBConnection {
-//     constructor() {
-//         this.pool = new Pool({
-//             host: process.env.HOST,
-//             user: process.env.DBUSER,
-//             password: process.env.PASSWORD,
-//             database: process.env.DATABASE,
-//             port: process.env.DBPORT,
-//         });
-//     }
-// }
-
 class DBConnection {
     constructor() {
         this.pool = new Pool({
-            host: 'csce-315-db.engr.tamu.edu',
-            user: 'team_2p',
-            password: 'pawmo',
-            database: 'team_2p_db',
-            port: 5432,
+            host: process.env.HOST,
+            user: process.env.DBUSER,
+            password: process.env.PASSWORD,
+            database: process.env.DATABASE,
+            port: process.env.DBPORT,
         });
     }
-}
+ }
 
 export default DBConnection;
